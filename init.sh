ce6b57c8d0af02297bb1370c6d98fb745a7a35c8
Alexandre Thomas <alexandre.thomas@closetome.fr>
2016-01-19 12:51:41 +0100

    Add prefix

--- init.sh
+++ init.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
+prefix=$1
 line=". $(pwd)/main"
+
 if [ $(grep "$line" ~/.bashrc | wc -l) == 1 ]; then 
 	echo "dotfiles already imported";
 else
 	echo "importing dotfiles";
+	echo "export prefix=${prefix}" >> ~/.bashrc;
 	echo ". $(pwd)/main" >> ~/.bashrc;
 fi
