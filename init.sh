f9668f3d8c073f19815305584d3313f950a252e5
Alexandre Thomas <alexandre.thomas@closetome.fr>
2016-01-19 13:21:44 +0100

    Pretty colors

--- init.sh
+++ init.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 prefix=$1
+
+if [ "$prefix" == "" ]; then
+    printf  "\033[0;31merror: \033[0mnot enough argument. define computer name\n"
+    exit 1
+fi
+
 line=". $(pwd)/main"
 
 if [ $(grep "$line" ~/.bashrc | wc -l) == 1 ]; then 
