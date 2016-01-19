81f5dc4ab379700993c1e67c8df3143ee03cfa65
Alexandre Thomas <alexandre.thomas@closetome.fr>
2016-01-19 12:12:39 +0100

    Add init script

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+line=". $(pwd)/main"
+if [ $(grep "$line" ~/.bashrc | wc -l) == 1 ]; then 
+	echo "dotfiles already imported";
+else
+	echo "importing dotfiles";
+	echo ". $(pwd)/main" >> ~/.bashrc;
+fi
