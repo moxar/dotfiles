3d9d813bc618553511a55f245033319e73e33d76
Alexandre Thomas <alexandre.thomas@closetome.fr>
2016-01-20 15:18:35 +0100

    Remove purple

--- init.sh
+++ init.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-prefix=$1
-
-if [ "$prefix" == "" ]; then
-    printf  "\033[0;31merror: \033[0mnot enough argument. define computer name\n"
-    exit 1
-fi
-
 line=". $(pwd)/main"
 
 if [ $(grep "$line" ~/.bashrc | wc -l) == 1 ]; then 
 	echo "dotfiles already imported";
 else
 	echo "importing dotfiles";
-	echo "export prefix=${prefix}" >> ~/.bashrc;
 	echo ". $(pwd)/main" >> ~/.bashrc;
 fi
