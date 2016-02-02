a39a81ce0de8848230064a55fff6efb588e6a13c
Alexandre Thomas <alexandre.thomas@closetome.fr>
2016-02-02 10:54:24 +0100

    Add GOPATH location to init script

--- init.sh
+++ init.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
 line=". $(pwd)/main"
+. $(pwd)/colors
 
 if [ $(grep "$line" ~/.bashrc | wc -l) == 1 ]; then 
-	echo "dotfiles already imported";
+	echo "${GREEN}dotfiles already imported";
 else
-	echo "importing dotfiles";
-	echo ". $(pwd)/main" >> ~/.bashrc;
+	echo "${GREEN}importing dotfiles";
+	echo $line >> ~/.bashrc;
+fi
+
+if [ $(grep "GOPATH=" ~/.bashrc | wc -l) == 1 ]; then
+	echo "${GREEN}GOPATH already set";
+else
+	read -p "`echo -e ${DEFAULT}'define GOPATH location: \n\b'`" GOPATH
+	if [ $(find $GOPATH -type d 2>/dev/null | wc -l) -eq 0 ]; then
+		echo "${RED}error: ${DEFAULT}no such file or directory \"$GOPATH\"";
+	else
+		echo "${GREEN}exporting GOPATH";
+	fi
+	echo "export GOPATH=$GOPATH" >> ~/.bashrc;
 fi
