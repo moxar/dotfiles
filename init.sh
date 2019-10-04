7fc7f76997b58d1f19153fd3b0d9177453b0fef0
Alexandre Thomas <alexandre.thomas@outlook.fr>
2019-10-04 18:53:33 +0200

    Set diff-so-fancy in stack

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+mkdir ~/.bin
+curl -o ~/.bin/diff-so-fancy https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy
+chmod +x ~.bin/diff-so-fancy
+
 line=". $(pwd)/main"
 echo $line >> ~/.bashrc;
 
